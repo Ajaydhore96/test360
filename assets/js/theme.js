caf576fc57cb8f5b9ecfd2cbe0e619cf6a791897
Ajaydhore <111976287+Ajaydhore96@users.noreply.github.com>
2026-05-28 17:16:45 +0530

    new

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -325,11 +325,11 @@
 
     if ($(".wow").length) {
         var wow = new WOW({
-            boxClass: "wow", // animated element css class (default is wow)
-            animateClass: "animated", // animation css class (default is animated)
-            offset: 50, // distance to the element when triggering the animation (default is 0)
-            mobile: true, // trigger animations on mobile devices (default is true)
-            live: true, // act on asynchronously loaded content (default is true)
+            boxClass: "wow",
+            animateClass: "animated",
+            offset: 100,
+            mobile: false,
+            live: false,
         });
         wow.init();
     }
